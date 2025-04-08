3aa9a7df0720678e07e115c9a49291deb05001d8
MarioBonifasius <112919371+MarioBonifasius@users.noreply.github.com>
2025-04-08 13:03:21 +0700

    .

--- crud-app/app/Http/Controllers/DashboardController.php
+++ crud-app/app/Http/Controllers/DashboardController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        return view('dashboard');
+        return view(view:'dashboard');
     }
 }
